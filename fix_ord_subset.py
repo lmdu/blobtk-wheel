4dc0512f3f7caed2723a1178e469b90a7cd3c03e
Lianming Du <adullb@qq.com>
2025-11-10 20:22:30 +0800

    Update fix_ord_subset.py

--- fix_ord_subset.py
+++ fix_ord_subset.py
@@ -5,10 +5,10 @@ config_file = 'rust/Cargo.toml'
 with open(config_file) as fh:
 	lines = fh.readlines()
 
-with open(config_file, 'w') as fh:
+with open(config_file, 'w') as fw:
 	for line in lines:
 		if line.strip() == '[dependencies]':
-			print(line, end='')
-			print('ord_subset = "3.1.2"')
+			print(line, file=fw, end='')
+			print('ord_subset = "3.1.2"', file=fw)
 		else:
-			print(line, end='')
+			print(line, file=fw, end='')
