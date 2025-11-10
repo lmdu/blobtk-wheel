56f418a95d41c5b6ccea4db93673bb3bd906d3c1
Lianming Du <adullb@qq.com>
2025-11-10 21:29:03 +0800

    Update fix_ord_subset.py

--- fix_ord_subset.py
+++ fix_ord_subset.py
@@ -10,6 +10,6 @@ with open(config_file, 'w') as fw:
 		if line.strip() == '[dependencies]':
 			print(line, file=fw, end='')
 			print('ord_subset = "3.1.2"', file=fw)
-			pirnt('openssl-sys = "0.9.111"', file=fw)
+			print('openssl-sys = "0.9.111"', file=fw)
 		else:
 			print(line, file=fw, end='')
